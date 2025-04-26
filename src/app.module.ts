4ea9c47c19df8fbb3507fa7914f0ea8992527592
Alejandro <alexaguilar281@gmail.com>
2025-04-26 10:10:10 +0200

    feat: :sparkles: bull mq config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,21 @@ import { APP_PIPE } from '@nestjs/core';
 import { GlobalPipes } from './config/validation-pipes';
 import { ConfigModule } from '@nestjs/config';
 import { validateEnv } from './config/envs/env.validation';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BullModule } from '@nestjs/bullmq';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       validate: validateEnv,
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_MONGO_URL!),
+    BullModule.forRoot({
+      connection: {
+        host: process.env.BULL_MQ,
+        port: Number(process.env.BULL_MQ_PORT),
+      },
     }),
   ],
   providers: [
